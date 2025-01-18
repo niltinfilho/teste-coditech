4dcb975e2956539ee6d9f6b9b95e113f6c09ea89
Nilton Filho <niltonfilho115@gmail.com>
2025-01-17 22:21:48 -0300

    feat: add hero and menu section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif']
+      }
     },
-    plugins: [],
-  }
+    colors: {
+      "golden": "#A57748",
+      "golden-light": "#B69B6C",
+      "golden-dark": "#925F31",
+      "white": "#FFF",
+      "transparent": "transparent",
+    },
+    backgroundImage: {
+      "hero": "url('/assets/advogados-associados-hero.jpg')"
+    }
+  },
+  plugins: [],
+};
