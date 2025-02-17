7ee66bfeb41a8ce0c06fa7c6e2379ec0d59966e8
Nilton Filho <101483210+niltinfilho@users.noreply.github.com>
2025-02-16 23:07:33 -0300

    estrutura

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,27 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        
-      }
+        "sans": ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        "btn": "0 0 10px 10px #0FBAC044",
+        "btn-hover": "0 0 15px 15px #0FBAC044",
+      },
     },
     colors: {
-      
+      "primary": "#0FBAC0",
+      "secondary": "#0FBAC0bb",
+      "tertiary": "#ffffff44",
+      "quaternary": "#cccccc",
+
+      "bg-primary": "#060606",
+      "bg-secondary": "#050505",
+      "bg-tertiary": "#111111",
+
+      "white": "#ffffff",
     },
     backgroundImage: {
-      
+
     }
   },
   plugins: [],
